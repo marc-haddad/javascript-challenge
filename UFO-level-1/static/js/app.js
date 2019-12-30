bb2754c7666e0945b26246fcab3ccf4f1e121bef
Marc H <marc.omar.haddad@gmail.com>
2019-12-30 15:41:02 -0500

    Completed UFO-level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var inputField = d3.select("#datetime");
+var button = d3.select("#filter-btn")
 
-// YOUR CODE HERE!
+tableData.forEach(function(data) {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+inputField.on("change", function() {
+    var newText = d3.event.target.value;
+    var dateFilter = tableData.filter(date => date.datetime === newText);
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    dateFilter.forEach(function(date) {
+        var row = tbody.append("tr");
+        Object.entries(date).forEach(function([key, value]){
+            var cell = row.append("td");
+            cell.text(value); 
+        }); 
+    
+    });
+  });
+  
